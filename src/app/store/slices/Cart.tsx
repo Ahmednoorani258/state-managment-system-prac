a57b4ddbd84c1889bf722f1bcb0c9ccf44620ff3
muhammad ahmed noorani <ahmednoorani258@gmail.com>
2024-12-14 15:37:23 -0800

    update

---
+++ src/app/store/slices/Cart.tsx
@@ -0,0 +1,14 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState:[],
+    reducers:{
+        addCart (state, action){},
+        deleteCart (state, action){}
+    },    
+})
+
+export const {addCart, deleteCart} = cartSlice.actions  
+export default cartSlice.reducer
