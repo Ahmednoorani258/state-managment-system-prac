6539b195b640e64453760f2c25e871507dcabcb9
Muhammad ahmed noorani noorani <159555834+Ahmednoorani258@users.noreply.github.com>
2024-12-14 11:38:22 +0000

    store build update

---
+++ src/app/providers.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import {Provider as ReduxProvider}  from 'react-redux'
+import { store } from "@/app/store/store"
+
+export default function Provider(
+    {children,}:{
+        children:React.ReactNode
+    }
+){
+    return(
+        <ReduxProvider store={store} >{children}</ReduxProvider>
+    )
+}
